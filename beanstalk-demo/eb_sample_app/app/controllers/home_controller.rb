a1467023e17dfb2e03888bd4ef6cc428fb7ab1b0
Julien Simon <julien@julien.org>
2016-10-03 15:45:02 +0200

    Delete BeanstalkDemo

--- beanstalk-demo/eb_sample_app/app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def index
-  end
-
-  def about
-  end
-end
--- beanstalk-demo/eb_sample_app/config/initializers/aws_ses.rb
+++
@@ -1,9 +0,0 @@
-require 'net/smtp'
-
-module Net
-  class SMTP
-    def tls?
-      true
-    end
-  end
-end
