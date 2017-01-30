b16113b80ec644b5347f0b65ac228bbcb04084b9
EC2 Default User <ec2-user@ip-172-31-5-65.eu-west-1.compute.internal>
2017-01-30 14:32:23 +0000

    Add delete script

--- lambda_frameworks/firstlambda/deploy.sh
+++ lambda_frameworks/firstlambda/deploy.sh
@@ -2,10 +2,6 @@
 
 source ~/dev/aws/setAccountNumber.sh
 
-aws lambda delete-function --function-name lambda1 >& /dev/null
-
-zip -9 lambda1.zip lambda1.py
-
 aws lambda create-function --function-name lambda1 --handler lambda1.lambda_handler \
  --runtime python2.7 --zip-file fileb://lambda1.zip --memory-size 128 \
 --role arn:aws:iam::$ACCOUNT_NUMBER:role/lambda_basic_execution \
