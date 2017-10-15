cb1890d67cf7d2cf0a03b93668305b6b6df7f451
Julien Simon <julien@julien.org>
2017-10-15 14:46:14 +0000

    Initial version

--- mxnet/intro/download_inception.sh
+++
@@ -1,4 +0,0 @@
-wget http://data.dmlc.ml/models/imagenet/inception-bn/Inception-BN-symbol.json
-wget http://data.dmlc.ml/models/imagenet/inception-bn/Inception-BN-0126.params
-wget http://data.dmlc.ml/models/imagenet/synset.txt
-mv Inception-BN-0126.params Inception-BN-0000.params
