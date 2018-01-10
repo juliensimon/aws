a84740e6191dd7a1605182115b195f9b42b343bf
Julien Simon <julien@julien.org>
2018-01-10 23:11:28 +0100

    Add messages

--- AmazonAI/billboard/billboard.py
+++ AmazonAI/billboard/billboard.py
@@ -9,25 +9,25 @@ translate = tr.connect()
 polly = po.connect()
 
 text = re.detectText(rekognition, 'jsimon-public-us', 'billboard.jpg')
-print(text)
+print("Detected text: " + text)
 
 language = co.detectLanguage(comprehend, text)
-print(language)
+print("Detected language: " + language)
 
 po.speak(polly, text, voice='Brian')
 
 translated_text = tr.translateText(translate, text, language, 'es')
-print translated_text
+print("Spanish: " +  translated_text)
 po.speak(polly, translated_text, voice='Enrique')
 
 translated_text = tr.translateText(translate, text, language, 'pt')
-print translated_text
+print("Portuguese: " +  translated_text)
 po.speak(polly, translated_text, voice='Cristiano')
 
 translated_text = tr.translateText(translate, text, language, 'fr')
-print translated_text
+print("French: " + translated_text)
 po.speak(polly, translated_text, voice='Mathieu')
 
 translated_text = tr.translateText(translate, text, language, 'de')
-print translated_text
+print("German: " +  translated_text)
 po.speak(polly, translated_text, voice='Hans')
