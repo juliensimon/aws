5c6e9ac330f28f7d3dbf608e3139919305bda00e
Julien Simon <julien@julien.org>
2018-04-26 21:17:57 +0000

    Initial version

--- lambda_frameworks/chalice/resizer/post.sh
+++ lambda_frameworks/chalice/resizer/post.sh
@@ -1,7 +1,7 @@
 #export URL='http://localhost:8000'
 export URL='https://bg8phyixb4.execute-api.us-east-1.amazonaws.com/api/'
 
-export PIC='julien.jpg'
+export PIC='floppy.jpg'
 
-(echo -n '{"data": "'; base64 $PIC; echo '", "height": 32, "width": 32}') |
+(echo -n '{"data": "'; base64 $PIC; echo '", "height": 32 , "width": 32}') |
 curl -H "Content-Type: application/json" -d @-  $URL
