a8bcb48ff28ba992e271f21018fe8e134628f2ca
Julien Simon <julien@julien.org>
2018-05-14 15:25:29 +0000

    Python 3 + tabs

--- mxnet/intro/compare.py
+++ mxnet/intro/compare.py
@@ -8,8 +8,8 @@ def loadModel(modelname):
 	t1 = time.time()
 	sym, arg_params, aux_params = mx.model.load_checkpoint(modelname, 0)
 	t2 = time.time()
-        t = 1000*(t2-t1)
-        print("Loaded in %2.2f milliseconds" % t)
+	t = 1000*(t2-t1)
+	print("Loaded in %2.2f milliseconds" % t)
 	arg_params['prob_label'] = mx.nd.array([0])
 	mod = mx.mod.Module(symbol=sym)
 	mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))])
@@ -28,8 +28,8 @@ def prepareNDArray(filename):
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 	img = cv2.resize(img, (224, 224,))
 	img = np.swapaxes(img, 0, 2)
-    	img = np.swapaxes(img, 1, 2)
-    	img = img[np.newaxis, :]
+	img = np.swapaxes(img, 1, 2)
+	img = img[np.newaxis, :]
 	return mx.nd.array(img) 
 
 def predict(filename, model, categories, n, iter):
@@ -43,13 +43,13 @@ def predict(filename, model, categories, n, iter):
 		sum = sum + 1000000*(t2-t1)
 
 	print("Predicted in %2.0f microseconds" % (sum/iter))
-    	prob = model.get_outputs()[0].asnumpy()
+	prob = model.get_outputs()[0].asnumpy()
 	prob = np.squeeze(prob)
-    	sortedprobindex = np.argsort(prob)[::-1]
+	sortedprobindex = np.argsort(prob)[::-1]
 	topn = []
-    	for i in sortedprobindex[0:n]:
+	for i in sortedprobindex[0:n]:
         	topn.append((prob[i], categories[i]))
-    	return topn
+	return topn
 
 def init(modelname): 
 	model = loadModel(modelname)
@@ -62,12 +62,12 @@ inceptionv3,c = init("Inception-BN")
 
 filename = sys.argv[1] 
 
-iter = 100
+iter = 1
 
 print ("*** VGG16")
-print predict(filename,vgg16,c,5, iter)
+print (predict(filename,vgg16,c,5, iter))
 print ("*** ResNet-152")
-print predict(filename,resnet152,c,5, iter)
+print (predict(filename,resnet152,c,5, iter))
 print ("*** Inception v3")
-print predict(filename,inceptionv3,c,5, iter)
+print (predict(filename,inceptionv3,c,5, iter))
 
